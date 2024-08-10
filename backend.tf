f46a9695517c32c068d6478cbe0ac6dda98f4f2b
fiyinking <100170529+fiyinking@users.noreply.github.com>
2024-08-10 13:50:51 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "myawsbucketfiyinmo"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    region         = "us-east-1"
+    dynamodb_table = "mofiyin-dynamo-db-table"
   }
 }
